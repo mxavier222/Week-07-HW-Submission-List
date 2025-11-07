a7af89585cf3e162ed7290327b60d7fd2b5ccbe7
mxavier <mxavier222@gmail.com>
2025-11-07 17:36:46 +0100

    homework-submission

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const students = [
+  "Samira",
+  "Sepideh",
+  "Evelina",
+  "Maglin",
+  "Anastasia",
+  "Sulakshana",
+  "Janaki",
+  "Irmak",
+  "Christy",
+  "Chanakarn",
+  "Narges",
+  "Yana",
+  "Seda",
+  "Vanitha",
+  "Afrin",
+  "Lucia",
+  "Hajra",
+];
+
+const teachers = ["Mack", "Kumaran", "Lukáš", "Tim"];
+
+function shuffle(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
+
+  const assignments = students.map((student, index) => ({
+    student,
+    teacher: teachers[index % teachers.length]
+  }));
+
+  let display = "Student          Teacher\n";
+  display += "----------------------------\n";
+
+  assignments.forEach(({ student, teacher }) => {
+    display += student.padEnd(13) + teacher + "\n";
+  });
+
+  document.getElementById("output").textContent = display;
+
